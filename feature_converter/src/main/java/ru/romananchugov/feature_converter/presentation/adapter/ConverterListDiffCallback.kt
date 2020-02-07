77696e5d477d03ca2ad47ec478f3222cab2ff114
RomanAnchugov <romagn@mail.ru>
2020-02-07 22:23:32 +0300

    [ConverterList] A21 - Add diff utils for next feature

--- feature_converter/src/main/java/ru/romananchugov/feature_converter/presentation/adapter/ConverterListDiffCallback.kt
+++ feature_converter/src/main/java/ru/romananchugov/feature_converter/presentation/adapter/ConverterListDiffCallback.kt
@@ -1,16 +1,16 @@
 package ru.romananchugov.feature_converter.presentation.adapter
 
 import androidx.recyclerview.widget.DiffUtil
-import ru.romananchugov.feature_converter.presentation.model.ConverterPresentationModel
+import ru.romananchugov.feature_converter.presentation.model.ConverterCurrencyWithFlagItem
 import ru.romananchugov.revoluttest.presentation.model.DisplayableItem
 
 class ConverterListDiffCallback: DiffUtil.ItemCallback<DisplayableItem>() {
     override fun areItemsTheSame(oldItem: DisplayableItem, newItem: DisplayableItem): Boolean {
-        return false
+        return oldItem == newItem
     }
 
     override fun areContentsTheSame(oldItem: DisplayableItem, newItem: DisplayableItem): Boolean {
-        return false
+        return (oldItem as ConverterCurrencyWithFlagItem).getCurrencyAbbreviation() == (newItem as ConverterCurrencyWithFlagItem).getCurrencyAbbreviation()
     }
 
 
